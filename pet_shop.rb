bb28aa3acd40fe8c591834c18f36738d0e51d105
Jack Quigley <jackaquigley@gmail.com>
2020-03-15 13:17:15 +0000

    total_cash test now works

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,9 @@
-def pet_shop_name(place)
-  return place[:name]
+def pet_shop_name(location)
+  return location[:name]
+end
+
+def total_cash(location)
+  sum = 0
+  sum += location[:admin][:total_cash]
+  return sum
 end
