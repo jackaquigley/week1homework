4ba5a53d91a201b909d801913ac8175813f96a82
Jack Quigley <jackaquigley@gmail.com>
2020-03-15 16:38:38 +0000

    add_or_remove_cash attempt added

--- pet_shop.rb
+++ pet_shop.rb
@@ -7,3 +7,13 @@ def total_cash(location)
   sum += location[:admin][:total_cash]
   return sum
 end
+
+def add_or_remove_cash(location, cash_to_add)
+cash = 0
+for sum in location[:admin]
+  for total in sum[:total_cash]
+  cash += total
+end
+end
+return cash
+end
